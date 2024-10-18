eafe98b8a6e5f3b5f16d51dc8eb3a04536aeeb54
swethadurai2115 <swethadurai2115@gmail.com>
2024-10-18 16:26:13 +0530

    commit

--- ecommerce-frontend/src/pages/HomePage.js
+++ ecommerce-frontend/src/pages/HomePage.js
@@ -1,42 +1,24 @@
-import { Fragment, useEffect, useState } from 'react';
-import ProductCard from '../components/ProductCard';
+import {Fragment, useEffect, useState} from 'react'
+import ProductCard from '../components/ProductCard'
 import { useSearchParams } from 'react-router-dom';
 
 export default function Home() {
     const [products, setProducts] = useState([]);
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams, setSearchParams] =  useSearchParams()
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                const response = await fetch(`${process.env.REACT_APP_API_URL}/products?${searchParams}`);
-                
-                // Check if the response is OK
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
+        fetch(process.env.REACT_APP_API_URL+'/products?'+searchParams)
+        .then(res => res.json())
+        .then( res => setProducts(res.products))
+    },[searchParams])
 
-                const data = await response.json();
-                setProducts(data.products);
-            } catch (error) {
-                console.error('Fetch error:', error);
-            }
-        };
+    return <Fragment>
+        <h1 id="products_heading">Latest Products</h1>
 
-        fetchProducts();
-    }, [searchParams]);
-
-    return (
-        <Fragment>
-            <h1 id="products_heading">Latest Products</h1>
-
-            <section id="products" className="container mt-5">
-                <div className="row">
-                    {products.map(product => (
-                        <ProductCard key={product.id} product={product} /> // Ensure each ProductCard has a unique key
-                    ))} 
-                </div>
-            </section>
-        </Fragment>
-    );
+        <section id="products" className="container mt-5">
+        <div className="row">
+            {products.map(product =><ProductCard product={product} />)} 
+        </div>
+        </section>
+    </Fragment>
 }
